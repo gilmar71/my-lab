a0e6a568f33dc78f30e224465eecc5f09de9a35f
gilmar71 <gilmar_770@hotmail.com>
2022-10-16 19:15:21 -0300

    feat: inicio

--- components/sections/home/banner/styles.ts
+++ components/sections/home/banner/styles.ts
@@ -2,20 +2,7 @@ import styled from 'styled-components';
 
 export const Banner = styled.section`
   padding-top: 200px;
-  .bg {
-    /* background-image: url('/images/home-banner.jpg'); */
-    width: 100%;
-    height: 100vh;
-    background-size: cover;
-    background-repeat: no-repeat;
-    background-position: center center;
-    position: relative;
-  }
   .container {
-    h1,
-    h2 {
-      color: white;
-    }
   }
 
   @media only screen and (max-width: 1600px) {
